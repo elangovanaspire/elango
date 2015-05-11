ba08247e25b26de3844838bfef5e9588af2fac90
maddyelan <maddyelan@gmail.com>
2015-05-11 07:57:30 +0530

    unit and e2e test case added

--- app/test/e2e/scenarios.js
+++ app/test/e2e/scenarios.js
@@ -3,6 +3,44 @@
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
 describe('F1 Race App', function() {
+    describe('Drivers view', function(){
+                
+    beforeEach(function() {
+      browser.get('index.html');
+    });   
+    it('Total Drivers', function(){
+           var drivers = element.all(by.repeater('driver in driversList'));
+           var searchText = element(by.model('nameFilter'));
+           
+           expect(drivers.count()).toBe(23);
+           
+           searchText.sendKeys('alonso');
+           expect(drivers.count()).toBe(1);
+           
+           searchText.clear();
+           
+           searchText.sendKeys('Massa');
+           expect(drivers.count()).toBe(1);
+           
+           searchText.clear();
+           
+           searchText.sendKeys('nico');
+           expect(drivers.count()).toBe(2);
+           
+       
+        });
+    }); 
+    
+    describe('Driver view', function() {
 
+        beforeEach(function() {
+          browser.get('index.html#/drivers/alonso');
+        });
+        
+        it('should display placeholder page with phoneId', function() {
+            expect(element(by.css('.driver-country')).getText()).toBe('Spanish');
+      });
+    
+    });
   
 });
--- app/test/unit/controllersSpec.js
+++ app/test/unit/controllersSpec.js
@@ -1,7 +1,16 @@
-'use strict';
+describe('F1 Race App', function() {    
+    describe('driversController', function() {  
+        
+                
+        beforeEach(module('F1FeederApp'));
 
-/* jasmine specs for controllers go here */
-describe('F1 Race App', function() {
+        beforeEach(inject(function($controller) {
+          scope = {};
+          ctrl = $controller('driversController', {$scope:scope});
+        }));
 
-  
+        it('should create "drivers" model with 23 drivers', function() {
+          expect(scope.driversList.length).toBe(0);
+        });
+    });
 });
